344e92961e544962f3530de8a15bd0a4c1a8278e
Shubham Sharma <shubh.luck@yahoo.in>
2024-08-30 13:45:05 -0400

    ODP-1880 Enable endpoint override option for S3FileResourceService to allow internal object store custom URLs

--- nifi-nar-bundles/nifi-aws-bundle/nifi-aws-processors/src/main/java/org/apache/nifi/processors/aws/s3/S3FileResourceService.java
+++ nifi-nar-bundles/nifi-aws-bundle/nifi-aws-processors/src/main/java/org/apache/nifi/processors/aws/s3/S3FileResourceService.java
@@ -21,7 +21,9 @@ import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
@@ -33,15 +35,18 @@ import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.context.PropertyContext;
 import org.apache.nifi.controller.AbstractControllerService;
 import org.apache.nifi.controller.ConfigurationContext;
+import org.apache.nifi.expression.ExpressionLanguageScope;
 import org.apache.nifi.fileresource.service.api.FileResource;
 import org.apache.nifi.fileresource.service.api.FileResourceService;
 import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.processors.aws.credentials.provider.service.AWSCredentialsProviderService;
 import org.apache.nifi.processors.aws.util.RegionUtilV1;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 
 import static org.apache.nifi.processors.aws.AbstractAWSCredentialsProviderProcessor.AWS_CREDENTIALS_PROVIDER_SERVICE;
 import static org.apache.nifi.processors.aws.util.RegionUtilV1.resolveS3Region;
@@ -64,13 +69,34 @@ public class S3FileResourceService extends AbstractControllerService implements 
             .fromPropertyDescriptor(RegionUtilV1.S3_REGION)
             .build();
 
+    public static final PropertyDescriptor ENDPOINT_OVERRIDE = new PropertyDescriptor.Builder()
+            .name("Endpoint Override URL")
+            .description("Endpoint URL to use instead of the AWS default including scheme, host, port, and path. " +
+                         "The AWS libraries select an endpoint URL based on the AWS region, but this property overrides " +
+                         "the selected endpoint URL, allowing use with other S3-compatible endpoints.")
+            .expressionLanguageSupported(ExpressionLanguageScope.VARIABLE_REGISTRY)
+            .required(false)
+            .addValidator(StandardValidators.URL_VALIDATOR)
+            .build();
+
+    public static final PropertyDescriptor USE_PATH_STYLE_ACCESS = new PropertyDescriptor.Builder()
+            .name("use-path-style-access")
+            .displayName("Use Path Style Access")
+            .description("Path-style access can be enforced by setting this property to true. Set it to true if your endpoint does not support " +
+                         "virtual-hosted-style requests, only path-style requests.")
+            .allowableValues("true", "false")
+            .defaultValue("true")
+            .build();
+
     private static final List<PropertyDescriptor> PROPERTIES = Arrays.asList(
             BUCKET,
             KEY,
             S3_REGION,
-            AWS_CREDENTIALS_PROVIDER_SERVICE);
+            ENDPOINT_OVERRIDE,
+            AWS_CREDENTIALS_PROVIDER_SERVICE,
+            USE_PATH_STYLE_ACCESS);
 
-    private final Cache<Region, AmazonS3> clientCache = Caffeine.newBuilder().build();
+    private final Cache<String, AmazonS3> clientCache = Caffeine.newBuilder().build();
 
     private volatile PropertyContext context;
 
@@ -132,9 +158,24 @@ public class S3FileResourceService extends AbstractControllerService implements 
 
     protected AmazonS3 getS3Client(Map<String, String> attributes, AWSCredentialsProvider credentialsProvider) {
         final Region region = resolveS3Region(context, attributes);
-        return clientCache.get(region, ignored -> AmazonS3Client.builder()
-                .withRegion(region.getName())
-                .withCredentials(credentialsProvider)
-                .build());
+        String urlStr = StringUtils.trimToEmpty(context.getProperty(ENDPOINT_OVERRIDE).evaluateAttributeExpressions().getValue());
+
+        // Create a cache key based on the region and the endpoint URL
+        String cacheKey = (urlStr.isEmpty() ? region.getName() : region.getName() + "::" + urlStr);
+
+        return clientCache.get(cacheKey, ignored -> {
+            AmazonS3ClientBuilder builder = AmazonS3Client.builder()
+                    .withCredentials(credentialsProvider);
+
+            if (!urlStr.isEmpty()) {
+                builder.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(urlStr,
+                        region.getName()));
+                builder.setPathStyleAccessEnabled(context.getProperty(USE_PATH_STYLE_ACCESS).asBoolean());
+            } else {
+                builder.withRegion(region.getName());
+            }
+
+            return builder.build();
+        });
     }
 }
