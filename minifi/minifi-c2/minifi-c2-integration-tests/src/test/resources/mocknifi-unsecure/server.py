f6e968384daa0e1f38855f7fee3235a449371728
dishtikundra <dishti@acceldata.io>
2024-12-10 15:34:02 +0530

    ODP-2771 Refactor shebangs for python scripts to ambari-python-wrap

--- minifi/minifi-c2/minifi-c2-integration-tests/src/test/resources/mocknifi-unsecure/server.py
+++ minifi/minifi-c2/minifi-c2-integration-tests/src/test/resources/mocknifi-unsecure/server.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/usr/bin/env python
+#!/usr/bin/env ambari-python-wrap
 
 import logging
 
