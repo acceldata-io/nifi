560b255429bbc66153372cefdd34a017867d57b7
Manish Singh Mowall <manishsingh@acceldata.io>
2025-02-19 11:40:40 +0530

    ODP-3041: jakarta xml bind annotation fix in nifi-nar-bundles.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-security/src/main/java/org/apache/nifi/web/security/NiFiAuthenticationFilter.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-security/src/main/java/org/apache/nifi/web/security/NiFiAuthenticationFilter.java
@@ -29,8 +29,8 @@ import org.springframework.web.filter.GenericFilterBean;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
